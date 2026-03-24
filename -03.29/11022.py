901e586a5b9730eb028b3431d3393df36eb5dd35
laura0810328 <laura0810328@gmail.com>
2026-03-24 14:59:59 +0900

    Create 11022.py

---
+++ -03.29/11022.py
@@ -0,0 +1,9 @@
+n=int(input())
+list=[]
+for i in range(n):
+    x,y=map(int, input().split())
+    list.append(x)
+    list.append(y)
+
+for j in range(n):
+    print("Case #", j+1, ": ", list[j*2], " + ", list[j*2+1], " = ", list[j*2]+list[j*2+1], sep='')
